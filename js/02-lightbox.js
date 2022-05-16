1224ea23aa1d665f0b147a130c1b5ef7026a3756
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-05-16 20:34:09 +0300

    simpleLightbox creation
    all the js created, outer libraries linked

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+let simpleLightBoxInstance = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
